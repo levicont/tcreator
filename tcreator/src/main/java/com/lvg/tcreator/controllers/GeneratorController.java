1dbafd637315be7863b9c47e2bb64b4cbfa5cd12
levicont <victor84lg@mail.ru>
2020-03-02 07:11:02 +0200

    Removed groovy support

---
+++ tcreator/src/main/java/com/lvg/tcreator/controllers/GeneratorController.java
@@ -0,0 +1,45 @@
+package com.lvg.tcreator.controllers;
+
+import com.lvg.tcreator.managers.OrderManager;
+import com.lvg.tcreator.managers.TestManager;
+import com.lvg.tcreator.models.Order;
+import com.lvg.tcreator.models.Test;
+import com.lvg.tcreator.utils.DateUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * Created by Victor Levchenko (LVG Corp.) on 29.02.2020.
+ */
+@Controller
+public class GeneratorController {
+    @RequestMapping(value="/generator", method= RequestMethod.GET)
+    public String generator(Model model, @RequestParam(value="method",required=false) String method){
+        if (method == null)
+            return "redirect:/";
+        model.addAttribute("ndtMethod", method);
+        model.addAttribute("order", OrderManager.getDefaultOrder(method));
+        return "generator";
+    }
+
+    @RequestMapping(value="/generator", method=RequestMethod.POST)
+    public String report(@Valid @ModelAttribute Order order, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors())
+            return "generator";
+
+        model.addAttribute("ndtMethod", order.getNdtMethod().toString());
+        model.addAttribute("dateOrder", DateUtil.formatDate(order.getDate()));
+        TestManager tm = new TestManager(order);
+        List<Test> testList = tm.createTestList();
+        model.addAttribute("tests", testList);
+        return "report";
+    }
+}
