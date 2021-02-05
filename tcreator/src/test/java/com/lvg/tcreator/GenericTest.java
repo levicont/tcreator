42882e4ef189675a144a5567d2050785f003c258
levicont <levicont@yahoo.com>
2021-02-05 10:23:47 +0200

    Upd test 8

--- tcreator/src/test/java/com/lvg/tcreator/GenericTest.java
+++ tcreator/src/test/java/com/lvg/tcreator/GenericTest.java
@@ -1,7 +1,9 @@
 package com.lvg.tcreator;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
