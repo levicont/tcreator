3a8fe0a3c1c862cdc1fa4246772f0d5cbdac5bde
levicont <victor84lg@mail.ru>
2021-02-08 20:02:47 +0200

    Updated QuestionController

--- tcreator/src/main/java/com/lvg/tcreator/controllers/QuestionController.java
+++ tcreator/src/main/java/com/lvg/tcreator/controllers/QuestionController.java
@@ -1,14 +1,19 @@
 package com.lvg.tcreator.controllers;
 
+import com.lvg.tcreator.models.NdtMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static com.lvg.tcreator.config.R.GlobalAttributes.BODY_TEMPLATE_ATTRIBUTE;
+
 @Controller
-public class QuestionsController {
+public class QuestionController {
 
     @GetMapping("/questions")
     String getAllQuestions(Model model){
+        model.addAttribute(BODY_TEMPLATE_ATTRIBUTE, "questions");
+        model.addAttribute("questionNdtMethod", NdtMethod.VT);
         return "index";
     }
 }
