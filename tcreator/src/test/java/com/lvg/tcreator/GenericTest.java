42219b36a40ec2bc4844666596adb8d86289039f
levicont <levicont@yahoo.com>
2021-02-08 11:58:02 +0200

    Updated test with data migration

--- tcreator/src/test/java/com/lvg/tcreator/GenericTest.java
+++ tcreator/src/test/java/com/lvg/tcreator/GenericTest.java
@@ -1,24 +1,36 @@
 package com.lvg.tcreator;
 
+import com.lvg.tcreator.persistence.repositories.ExamRepository;
+import com.lvg.tcreator.persistence.repositories.OrderRepository;
+import com.lvg.tcreator.persistence.repositories.QuestionRepository;
+import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(locations="classpath:test.properties")
 public abstract class GenericTest {
 
-    @PersistenceContext
-    protected EntityManager em;
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private ExamRepository examRepository;
 
-    protected void deleteAllQuestionsFromDB(){
-        Query query = em.createQuery("delete from QuestionDB");
-        query.executeUpdate();
+    @Before
+    public void deleteAllQuestionsFromDB(){
+        questionRepository.deleteAll();
+        examRepository.deleteAll();
+        orderRepository.deleteAll();
     }
+
+
 }
