de783a21c1de81a679eebfd49717afe3f8dcc7e1
levicont <victor84lg@mail.ru>
2021-02-10 20:35:19 +0200

    Updated QuestionController - 2

--- tcreator/src/main/java/com/lvg/tcreator/controllers/QuestionController.java
+++ tcreator/src/main/java/com/lvg/tcreator/controllers/QuestionController.java
@@ -2,11 +2,13 @@ package com.lvg.tcreator.controllers;
 
 import com.lvg.tcreator.models.NdtMethod;
 import com.lvg.tcreator.models.TestTypes;
+import com.lvg.tcreator.persistence.models.QuestionDB;
 import com.lvg.tcreator.persistence.services.QuestionDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.*;
 
@@ -20,9 +22,7 @@ public class QuestionController {
 
     @GetMapping("/questions")
     String getAllQuestions(Model model){
-
         Map<NdtMethod, Map<TestTypes, Integer>> ndtMethodCountMap = new TreeMap<>();
-
         Arrays.asList(NdtMethod.values()).forEach(method ->{
             Map<TestTypes, Integer> testTypeCountMap = new TreeMap<>();
             Arrays.asList(TestTypes.values()).forEach(testType->{
@@ -31,12 +31,23 @@ public class QuestionController {
             });
             ndtMethodCountMap.put(method, testTypeCountMap);
         });
-
         model.addAttribute(BODY_TEMPLATE_ATTRIBUTE, "questions");
         model.addAttribute("questionNdtMethod", NdtMethod.VT);
         model.addAttribute("ndtMethodCountMap", ndtMethodCountMap);
-
-
         return "index";
+    }
+
+    @GetMapping(value="/questions/{ndtMethod}/{testType}")
+    String getQuestionsByNdtMethodAndTestType(@PathVariable NdtMethod ndtMethod, @PathVariable TestTypes testType,
+                                              Model model){
+        List<QuestionDB> questions = questionDbService.findByNdtMethodTestTypes(ndtMethod, testType);
+        if (questions.isEmpty())
+            return "redirect:/questions";
+        model.addAttribute(BODY_TEMPLATE_ATTRIBUTE, "questionsByNdtMethodAndTestType");
+        model.addAttribute("ndtMethod", ndtMethod);
+        model.addAttribute("testType", testType);
+        model.addAttribute("questions", questions);
+        return "index";
+        
     }
 }
